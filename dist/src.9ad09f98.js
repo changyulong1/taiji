ca164263bbe25fe7b33f28abd590dc5532ad71d5
xiaocao <853044263@qq.com>
2022-01-09 22:21:15 +0800

    更新1

--- dist/src.9ad09f98.js
+++ dist/src.9ad09f98.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58241" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58726" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/admin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/src.9ad09f98.js.map
+//# sourceMappingURL=fuck/src.9ad09f98.js.map
